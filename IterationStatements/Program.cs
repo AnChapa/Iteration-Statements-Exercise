cf1d32eb206fa8d7ee0e865cd5e790f86e88c7de
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-17 10:53:08 -0600

    initial commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace IterationStatements
+﻿using System.Globalization;
+
+namespace IterationStatements
 {
     public class Program
     {
@@ -27,7 +29,114 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Printing numbers from 1000 to -1000:\n-------------------------------------------\n");
+            PrintThousandToNegThou();
+            Console.WriteLine("\n-------------------------------------------");
+            Console.WriteLine("Printing numbers from 3 to 999:\n-------------------------------------------\n");
+            PrintThreeToNNN();
+
+            var numbers = new List<int>();
+            Console.WriteLine("\nGive me a number:");
+            numbers.Add(int.Parse(Console.ReadLine()));
+            Console.WriteLine("\nGive me another number:");
+
+            numbers.Add(int.Parse(Console.ReadLine()));
+            CheckEqual(numbers);
+            CheckIsEven(numbers);
+            CheckIsPos(numbers);
+
+            Console.WriteLine("\nWhat is your age:");
+            int age = int.Parse(Console.ReadLine());
+            CanVote(age);
+
+            Console.WriteLine("\nWhat number would you like to check:");
+            age = int.Parse(Console.ReadLine());
+            ISWithinTen(age);
+            MultTable(age);
+            Console.WriteLine("\n");
+        }
+
+        public static void PrintThousandToNegThou()
+        {
+            for(int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        public static void PrintThreeToNNN()
+        {
+            for(int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void CheckEqual(List<int> numbers)
+        {
+            int num1 = numbers[0];
+            int num2 = numbers[1];
+            if(num1 == num2)
+            {
+                Console.WriteLine($"\n{num1} is equal to {num2}\n");
+            }
+            else
+                Console.WriteLine($"\n{num1} is not equal to {num2}\n");
+        }
+
+        public static void CheckIsEven(List<int> numbers)
+        {
+            foreach(int i in numbers)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine($"{i} is an Even number");
+                }
+                else
+                    Console.WriteLine($"{i} is an Odd number");
+            }
+        }
+
+        public static void CheckIsPos(List<int> numbers)
+        {
+            foreach (int i in numbers)
+            {
+                if (i >= 0)
+                {
+                    Console.WriteLine($"{i} is a Positive number");
+                }
+                else
+                    Console.WriteLine($"{i} is a Negative number");
+            }
+        }
+
+        public static void CanVote(int num)
+        {
+            if(num >= 18)
+            {
+                Console.WriteLine("Congrats, you can vote!");
+            }
+            else
+                Console.WriteLine("Sorry, you cannot vote.");
+        }
+
+        public static void ISWithinTen(int num)
+        {
+            if(num <= 10 && num >= -10)
+            {
+                Console.WriteLine($"{num} is within 10 and -10");
+            }
+            else
+                Console.WriteLine($"{num} is not within 10 and -10");
+        }
+
+        public static void MultTable(int num)
+        {
+            Console.WriteLine($"\nThe multiples of {num} from 1 - 12 are:");
+            for(int i = 1; i < 13; i++)
+            {
+                Console.Write($"{num * i} ");
+            }
         }
     }
 }
